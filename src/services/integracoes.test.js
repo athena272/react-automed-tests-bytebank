45c93de39171f4e8679113aa55da381c8ec16a1e
athena272 <guilhermera272@gmail.com>
2024-05-24 13:21:43 -0300

    Update integracoes.test.js

--- src/services/integracoes.test.js
+++ src/services/integracoes.test.js
@@ -23,7 +23,7 @@ const mockTransacao = [
         id: 1,
         transacao: 'Depósito',
         valor: '100',
-        data: '22/11/2022',
+        data: '21/11/2022',
         mes: 'Novembro',
     },
 ];
@@ -38,12 +38,29 @@ const mockRequisicao = (retorno) => {
     });
 };
 
+const mockRequisicaoErro = () => {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject();
+        }, 200);
+    });
+};
+
 describe('Requisições para API', () => {
     test('Deve retornar uma lista de transações', async () => {
         api.get.mockImplementation(() => mockRequisicao(mockTransacao));
 
         const transacoes = await buscaTransacoes();
         expect(transacoes).toEqual(mockTransacao);
+        expect(api.get).toHaveBeenCalledWith('/transacoes');
+    });
+
+    test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
+        api.get.mockImplementation(() => mockRequisicaoErro());
+
+        const transacoes = await buscaTransacoes();
+
+        expect(transacoes).toEqual([]);
         expect(api.get).toHaveBeenCalledWith('/transacoes');
     });
 });
